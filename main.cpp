d5a93a93ca9a012ddb70e740dab1ab5fc458f481
ExpressionlessMask <theemotionlessmask@gmail.com>
2024-05-15 01:11:41 -0400

    basics 1

--- main.cpp
+++ main.cpp
@@ -1,10 +1,36 @@
-#include "robot.cpp"
+#include "robot.h"
 #include "map_parser.cpp"
+
 
 
 int main(void)
 {
-    
+    robot R(10, 10, 0);
+    R.publishPose();
 
     return 0;
+}
+
+
+
+//Robot class from here out
+robot::robot(double pos_x, double pos_y, double theta) { //eh?
+    this->state[0] = pos_x;
+    this->state[1] = pos_y;
+    this->state[2] = theta;
+};
+
+
+void robot::changeState(int i, double val) {
+
+}
+
+void robot::updatePose(double pos_x, double pos_y, double theta) {
+    this->state[0] = pos_x;
+    this->state[1] = pos_y;
+    this->state[2] = theta;
+}
+
+void robot::publishPose() {
+    cout <<"(" << state[0] << ", " << state[1] << ", " << state[2] << ")" << endl;
 }
--- robot.h
+++ robot.h
@@ -2,14 +2,26 @@
 class robot 
 {
 private:
-    double lVel;
-    double omega;
+    double linear = 0.0;
+    double omega = 0.0;
+    double state[3];
+    static const int TIMESTEP = 1000;
 
 public:
-    void upd_acc();
-    void upd_vel();
+    robot(double, double, double);
 
+    void setLinear(double linear)
+    { this->linear = linear; }
+
+    void setOmega(double omega)
+    { this->omega = omega; }
+
+    void changeState(int, double);
+
+    void updatePose(double, double, double);
     //void getScan();
-
+    void publishPose();
 
 };
+
+//static const double TIMESTEP = 0.001;
